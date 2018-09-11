6ec6953eafd6c00518eabd83835ac66a344c4068
Jonas <N/A>
2018-09-11 14:17:20 +0200

    A View with a Data Collection

--- AspNetVideoCore/Controllers/HomeController.cs
+++ AspNetVideoCore/Controllers/HomeController.cs
@@ -11,7 +11,13 @@ namespace AspNetVideoCore.Controllers
     {
         public ViewResult Index()
         {
-            var model = new Video { Id = 1, Title = "Shreck" };
+            var model = new List<Video>
+            {
+                new Video { Id = 1, Title = "Shreck" },
+                new Video { Id = 2, Title = "Despicable Me" },
+                new Video { Id = 3, Title = "Megamind" }
+            };
+
             return View(model);
         }
     }
