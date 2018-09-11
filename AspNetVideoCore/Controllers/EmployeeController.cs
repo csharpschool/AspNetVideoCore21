a607aa9b13fbe48775423ea42d4ab777d6e1b6b0
Jonas <N/A>
2018-09-11 13:43:56 +0200

    Attribute Routing: Hardcoded route names

--- AspNetVideoCore/Controllers/EmployeeController.cs
+++ AspNetVideoCore/Controllers/EmployeeController.cs
@@ -1,22 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspNetVideoCore.Controllers
 {
+    [Route("employee")]
     public class EmployeeController
     {
+        [Route("name")]
         public string Name()
         {
             return "Jonas";
         }
 
+        [Route("country")]
         public string Country()
         {
             return "Sweden";
         }
 
+        [Route("")]
         public string Index()
         {
             return "Hello from Employee";
