d9055ecb76cf637b5e712008851dc2a4bc93c4a4
Jonas <N/A>
2018-09-11 13:22:58 +0200

    Serving Up Static Files

--- AspNetVideoCore/Startup.cs
+++ AspNetVideoCore/Startup.cs
@@ -41,6 +41,8 @@ namespace AspNetVideoCore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync(msg.GetMessage());
