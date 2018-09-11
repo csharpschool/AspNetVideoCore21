216b2c356f3f19dc519e8c7ebb759a9b62bc8c64
Jonas <N/A>
2018-09-11 13:49:27 +0200

    Attribute Routing: On controller level

--- AspNetVideoCore/Controllers/EmployeeController.cs
+++ AspNetVideoCore/Controllers/EmployeeController.cs
@@ -6,23 +6,19 @@ using System.Threading.Tasks;
 
 namespace AspNetVideoCore.Controllers
 {
-    [Route("employee")]
+    [Route("company/[controller]/[action]")]
     public class EmployeeController
     {
-        [Route("[action]")]
         public string Name()
         {
             return "Jonas";
         }
 
-        [Route("[action]")]
         public string Country()
         {
             return "Sweden";
         }
 
-        [Route("")]
-        [Route("[action]")]
         public string Index()
         {
             return "Hello from Employee";
