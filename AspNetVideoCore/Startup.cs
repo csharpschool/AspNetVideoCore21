fc0666165d7e7b842aea18aa2ecafe4276891138
Jonas <N/A>
2018-09-11 12:48:08 +0200

    Adding a Configuration Service

--- AspNetVideoCore/Startup.cs
+++ AspNetVideoCore/Startup.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetVideoCore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +40,8 @@ namespace AspNetVideoCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello, from My World!");
+                var message = Configuration["Message"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
