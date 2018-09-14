6beb8803117056a0a01ddd01d16e6add3558ae38
Jonas <N/A>
2018-09-14 15:16:16 +0200

    User Registration

--- AspNetVideoCore/Migrations/20180914125251_IdentityTables.Designer.cs
+++ AspNetVideoCore/Migrations/20180914125251_IdentityTables.Designer.cs
@@ -4,14 +4,16 @@ using AspNetVideoCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetVideoCore.Migrations
 {
     [DbContext(typeof(VideoDbContext))]
-    partial class VideoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180914125251_IdentityTables")]
+    partial class IdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
